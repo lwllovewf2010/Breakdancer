654254c17e5849bbfa7dfe337dfd2bd5caa2f54e
Jimmy <jimmyhales@outlook.com>
2012-11-20 12:18:52 -0700

    Moved Nozomi's awesome cube to seperate Activity.  Need to make button on main screen go to the cube activity.

--- gen/com/nozimmy/breakdancer/R.java
+++ gen/com/nozimmy/breakdancer/R.java
@@ -14,7 +14,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int BeginDance=0x7f070000;
+        public static final int Settings=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_breakdancer=0x7f030000;
@@ -24,8 +26,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int begin_dance=0x7f040003;
         public static final int main_title=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int settings=0x7f040004;
     }
     public static final class style {
         /** 
--- src/com/nozimmy/breakdancer/Breakdancer.java
+++ src/com/nozimmy/breakdancer/Breakdancer.java
@@ -9,62 +9,17 @@ import android.view.View.OnTouchListener;
 
 public class Breakdancer extends Activity implements OnTouchListener
 {
-	MyGLView myGLView;
-	float initPos;
-	float dragDist;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) 
+	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		myGLView = new MyGLView(this);
-		myGLView.setOnTouchListener((OnTouchListener) this);
-		setContentView(myGLView);
+		setContentView(R.layout.activity_breakdancer);
 	}
 
 	@Override
-	protected void onResume()
+	public boolean onTouch(View v, MotionEvent event) 
 	{
-		super.onResume();
-		myGLView.onResume();
-	}
-	  
-	@Override
-	protected void onPause()
-	{
-		super.onPause();
-		myGLView.onPause();
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) 
-	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_breakdancer, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onTouch(View v, MotionEvent event)
-	{
-		int action = event.getAction() & MotionEvent.ACTION_MASK;
-		switch (action) {
-		case MotionEvent.ACTION_DOWN:
-		case MotionEvent.ACTION_POINTER_DOWN:
-			dragDist = 0;
-			initPos = event.getX();
-			break;
-		case MotionEvent.ACTION_UP:
-		case MotionEvent.ACTION_POINTER_UP:
-		case MotionEvent.ACTION_OUTSIDE:
-		case MotionEvent.ACTION_CANCEL:
-		case MotionEvent.ACTION_MOVE:
-			dragDist = event.getX() - initPos;
-			initPos = event.getX();
-			break;
-		}
-		myGLView.rotateView(dragDist / 2);
-		
-		return true;
-	}
 }
