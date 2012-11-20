eba90b99b6d7b382b1b415ec15b1da2d40d7a5d9
Jimmy <jimmyhales@outlook.com>
2012-11-20 13:00:38 -0700

    Put a main screen.  Click the "dance" button to go to the awesome cube.

--- src/com/nozimmy/breakdancer/Breakdancer.java
+++ src/com/nozimmy/breakdancer/Breakdancer.java
@@ -2,18 +2,43 @@ package com.nozimmy.breakdancer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Breakdancer extends Activity implements OnTouchListener
 {
+	Button danceButton;
+	Button settingsButton;
+	
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_breakdancer);
+		
+		danceButton = (Button)findViewById(R.id.BeginDance);
+		danceButton.setOnClickListener(btnListener);
+		
+		settingsButton = (Button)findViewById(R.id.Settings);
+		settingsButton.setOnClickListener(btnListener);
 	}
+	
+	private OnClickListener btnListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			/*Toast.makeText(getBaseContext(),
+					((Button) v).getText() + "was pushed",
+					Toast.LENGTH_SHORT).show();*/
+			Intent intent = new Intent(Breakdancer.this, MotherCube.class);
+			startActivity(intent);
+		}
+	};
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) 
